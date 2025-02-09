8437f89bfaefa725ddc1bb6bc7dbf1883b87cc65
Egor Aristov <ef@efprojects.com>
2025-02-09 10:42:42 +0300

    extractor rewrite (use custom wrappers over playwright locator)

--- internal/extractors/pwextractor/pwextractor.go
+++ internal/extractors/pwextractor/pwextractor.go
@@ -1,8 +1,6 @@
 package pwextractor
 
 import (
-	"context"
-	_ "embed"
 	"fmt"
 	"github.com/egor3f/rssalchemy/internal/models"
 	"github.com/labstack/gommon/log"
@@ -10,14 +8,6 @@ import (
 	"maps"
 	"strings"
 	"time"
-)
-
-// Timeouts
-var (
-	defTimeout    = "50ms"
-	defOptInText  = playwright.LocatorInnerTextOptions{Timeout: pwDuration(defTimeout)}
-	defOptTextCon = playwright.LocatorTextContentOptions{Timeout: pwDuration(defTimeout)}
-	defOptAttr    = playwright.LocatorGetAttributeOptions{Timeout: pwDuration(defTimeout)}
 )
 
 var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
@@ -228,157 +218,4 @@ func (e *PwExtractor) Screenshot(task models.Task) (result *models.ScreenshotTas
 		return nil
 	})
 	return
-}
-
-type pageParser struct {
-	task       models.Task
-	page       playwright.Page
-	dateParser DateParser
-
-	// next fields only for debugging. Shit code, to do better later
-	postIdx  int
-	fieldIdx int
-}
-
-// must accepts arbitrary string and error and returns just string, also logs everything.
-// it is used for playwright functons that return both string and error to avoid boilerplate.
-// fieldIdx is convinient variable used only for logging purposes, looks like shit, maybe i'll do it better later.
-func (p *pageParser) must(s string, err error) string {
-	p.fieldIdx++
-	if err != nil {
-		log.Errorf("extract post field %d: %v", p.fieldIdx, err)
-		return ""
-	}
-	//log.Debugf("field=%d res=%.100s", p.fieldIdx, s)
-	return s
-}
-
-func (p *pageParser) parse() (*models.TaskResult, error) {
-	var result models.TaskResult
-	var err error
-
-	p.waitFullLoad()
-
-	result.Title, err = p.page.Title()
-	if err != nil {
-		return nil, fmt.Errorf("page title: %w", err)
-	}
-
-	iconUrl, err := p.page.Locator("link[rel=apple-touch-icon]").First().
-		GetAttribute("href", playwright.LocatorGetAttributeOptions{Timeout: pwDuration("100ms")})
-	if err != nil {
-		log.Warnf("page icon url: %v", err)
-	} else {
-		result.Icon = absUrl(iconUrl, p.page)
-	}
-
-	posts, err := p.page.Locator(p.task.SelectorPost).All()
-	if err != nil {
-		return nil, fmt.Errorf("post locator: %w", err)
-	}
-	if len(posts) == 0 {
-		return nil, fmt.Errorf("no posts on page")
-	}
-	log.Debugf("Posts count=%d", len(posts))
-
-	for _, post := range posts {
-		item, err := p.extractPost(post)
-		if err != nil {
-			log.Errorf("extract post fields: %v", err)
-			continue
-		}
-		if len(item.Title) == 0 || len(item.Link) == 0 || item.Created.IsZero() {
-			log.Warnf("post has no required fields, skip")
-			continue
-		}
-		result.Items = append(result.Items, item)
-	}
-
-	return &result, nil
-}
-
-func (p *pageParser) waitFullLoad() {
-	timeout := pwDuration("5s")
-	ctx, cancel := context.WithCancel(context.Background())
-
-	go func() {
-		err := p.page.WaitForLoadState(playwright.PageWaitForLoadStateOptions{
-			State:   playwright.LoadStateNetworkidle,
-			Timeout: timeout,
-		})
-		log.Debugf("WaitFor LoadState finished with %v", err)
-		cancel()
-	}()
-	go func() {
-		err := p.page.Locator(p.task.SelectorPost).Locator(p.task.SelectorTitle).Last().WaitFor(
-			playwright.LocatorWaitForOptions{
-				State:   playwright.WaitForSelectorStateVisible,
-				Timeout: timeout,
-			},
-		)
-		log.Debugf("WaitFor LOCATOR finished with %v", err)
-		cancel()
-	}()
-
-	<-ctx.Done()
-}
-
-func (p *pageParser) extractPost(post playwright.Locator) (models.FeedItem, error) {
-	p.fieldIdx = 0
-	p.postIdx++
-	var item models.FeedItem
-
-	item.Title = p.must(post.Locator(p.task.SelectorTitle).First().InnerText(defOptInText))
-	log.Debugf("---- POST: %s ----", item.Title)
-
-	item.Link = p.must(post.Locator(p.task.SelectorLink).First().GetAttribute("href", defOptAttr))
-	page, _ := post.Page()
-	item.Link = absUrl(item.Link, page)
-
-	if len(p.task.SelectorDescription) > 0 {
-		item.Description = p.must(post.Locator(p.task.SelectorDescription).First().InnerText(defOptInText))
-	}
-
-	item.AuthorName = p.must(post.Locator(p.task.SelectorAuthor).First().InnerText(defOptInText))
-
-	item.AuthorLink = p.must(post.Locator(p.task.SelectorAuthor).First().GetAttribute("href", defOptAttr))
-	item.AuthorLink = absUrl(item.AuthorLink, page)
-
-	if len(p.task.SelectorContent) > 0 {
-		item.Content = p.extractContent(post)
-	}
-
-	item.Enclosure = p.must(post.Locator(p.task.SelectorEnclosure).First().GetAttribute("src", defOptAttr))
-
-	createdDateStr := p.must(post.Locator(p.task.SelectorCreated).First().InnerText(defOptInText))
-	log.Debugf("date=%s", createdDateStr)
-	createdDate, err := p.dateParser.ParseDate(createdDateStr)
-	if err != nil {
-		log.Errorf("dateparser: %v", err)
-	} else {
-		item.Created = createdDate
-	}
-
-	return item, nil
-}
-
-//go:embed extract_post.js
-var extractPostScript string
-
-func (p *pageParser) extractContent(post playwright.Locator) string {
-	postContent := post.Locator(p.task.SelectorContent)
-	result, err := postContent.Evaluate(
-		extractPostScript,
-		nil,
-		playwright.LocatorEvaluateOptions{Timeout: pwDuration("1s")},
-	)
-	if err != nil {
-		log.Errorf("extract post content: evaluate: %v", err)
-		return p.must(postContent.TextContent(defOptTextCon))
-	}
-	resString, ok := result.(string)
-	if !ok {
-		log.Errorf("extract post content: result type mismatch: %v", result)
-	}
-	return resString
 }
