68f363285375be9fead5722c7c256e9c6af8ae11
Egor Aristov <ef@efprojects.com>
2025-02-22 21:39:46 +0300

    improve ux

--- frontend/wizard-vue/src/pages/WizardPage.vue
+++ frontend/wizard-vue/src/pages/WizardPage.vue
@@ -6,9 +6,9 @@ import Copyable from "@/components/Copyable.vue";
 import EditUrlModal from "@/components/EditUrlModal.vue";
 import {decodeUrl, encodeUrl, getScreenshotUrl} from "@/urlmaker";
 import {useWizardStore} from "@/stores/wizard.ts";
+import {debounce} from "es-toolkit";
 
 const store = useWizardStore();
-
 const existingLink = ref("");
 const link = ref("");
 const editModalVisible = ref(false);
@@ -24,6 +24,14 @@ watch(existingLink, async (value) => {
     alert(`Decoding error: ${e}`);
   }
 });
+
+watch(store.specs, debounce(() => {
+  if(store.formValid) {
+    generateLink();
+  } else {
+    link.value = "";
+  }
+}, 100));
 
 async function generateLink() {
   try {
@@ -43,7 +51,7 @@ function screenshot() {
 <template>
   <div class="wrapper">
     <SpecsForm class="specs-form"></SpecsForm>
-    <Btn :active="store.formValid" @click="generateLink">Generate link</Btn>
+<!--    <Btn :active="store.formValid" @click="generateLink">Generate link</Btn>-->
     <Btn :active="store.formValid" @click="screenshot">Screenshot</Btn>
     <Btn @click="editModalVisible = true">Edit existing task</Btn>
     <Btn @click="store.reset">Reset Form</Btn>
