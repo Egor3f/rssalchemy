49155f5c38385d91d5092c7e88a95e13a4d23acc
Egor Aristov <ef@efprojects.com>
2025-03-11 13:23:26 +0300

    logo

--- frontend/wizard-vue/src/App.vue
+++ frontend/wizard-vue/src/App.vue
@@ -3,9 +3,26 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div id="header">
+    <div id="logo">
+      <img src="./assets/logo.png" alt="logo"/>
+    </div>
+  </div>
   <RouterView />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+#header {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
 
+  margin: 5px 0 10px 0;
+
+  #logo {
+    img {
+      max-width: 300px;
+    }
+  }
+}
 </style>
