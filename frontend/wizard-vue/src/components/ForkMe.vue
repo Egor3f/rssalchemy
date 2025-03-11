ba17dcd680275189f5fa0026075b13fbdc9c44b7
Egor Aristov <ef@efprojects.com>
2025-03-11 15:43:46 +0300

    fork me on github

---
+++ frontend/wizard-vue/src/components/ForkMe.vue
@@ -0,0 +1,15 @@
+<template>
+  <a href="https://github.com/egor3f/rssalchemy" target="_blank">
+    <img decoding="async" width="149" height="149"
+         src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png"
+         class="attachment-full size-full" alt="Fork me on GitHub" loading="lazy">
+  </a>
+</template>
+
+<style scoped lang="scss">
+.attachment-full {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+</style>
