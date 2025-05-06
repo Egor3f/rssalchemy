b190ec3d04ed3214c98c4c6c609b05be7d93439c
Egor Aristov <ef@efprojects.com>
2025-05-06 19:01:18 +0300

    extract from field

---
+++ frontend/wizard-vue/src/components/RadioButtons.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import {getCurrentInstance} from "vue";
+import type {Enum} from "@/urlmaker/specs.ts";
+
+const {name, label, values} = defineProps<{
+  name: string
+  label: string,
+  values: Enum,
+}>();
+
+const componentId = 'field' + getCurrentInstance()?.uid;
+
+const model = defineModel();
+
+</script>
+
+<template>
+  <div class="field">
+    <span class="field-label"><label>{{ label }}</label></span>
+    <template class="value" v-for="enumValue in values">
+      <input
+        type="radio"
+        :name="name"
+        :value="enumValue.value"
+        :id="`${componentId}_${enumValue.value}`"
+        v-model="model"
+      />
+      <label class="radio-label" :for="`${componentId}_${enumValue.value}`">{{ enumValue.label }}</label>
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div.field {
+  margin: 0 0 8px 0;
+}
+.field-label {
+  font-size: 0.9em;
+  margin-right: 8px;
+}
+.radio-label {
+  font-size: 0.9em;
+}
+input, .radio-label, .field-label {
+  vertical-align: middle;
+}
+input {
+  margin-top: 0;
+}
+</style>
--- frontend/wizard-vue/src/stores/wizard.ts
+++ frontend/wizard-vue/src/stores/wizard.ts
@@ -1,5 +1,5 @@
 import {defineStore} from "pinia";
-import {emptySpecs, type SpecField, fields, type Specs} from "@/urlmaker/specs.ts";
+import {emptySpecs, type SpecField, fields, type Specs, type SpecValue} from "@/urlmaker/specs.ts";
 import {computed, reactive} from "vue";
 import {debounce} from "es-toolkit";
 
@@ -22,7 +22,7 @@ export const useWizardStore = defineStore('wizard', () => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(specs));
   }, 100);
 
-  function updateSpec(fieldName: keyof Specs, newValue: string) {
+  function updateSpec(fieldName: keyof Specs, newValue: SpecValue) {
     specs[fieldName] = newValue;
     updateLocalStorage();
   }
